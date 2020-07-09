440506cc533f8d5901487f8bc82d18b19355d598
DimKush <demonmadman228@gmail.com>
2020-07-09 13:12:18 +0300

    01-BOOST | create basic functionality for file processing

--- path_worker/path_worker.h
+++ path_worker/path_worker.h
@@ -1,9 +1,15 @@
 #pragma once 
 
+#include <boost/filesystem/operations.hpp>
+#include <boost/filesystem/fstream.hpp>
 
+#include <iostream>
+#include <thread>
 
 namespace PathWorker
 {
-    void try_to_open_directories(int argc, char **argv);
-
+    namespace fs = boost::filesystem;
+    std::list<fs::path> try_to_open_directories(int argc, char **argv);
+    void process_directory(fs::path const & workingPath);
+    void process_file(std::string const & fileName);
 }
